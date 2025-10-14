a781d51bc4456277a11deeab18da1c950a3e8581
Adeebaabkhan <97935276+Adeebaabkhan@users.noreply.github.com>
2025-10-14 17:24:36 +0530

    Create NonprofitCertificateGenerator class for generating nonprofit certificates

---
+++ js/nonprofitCertificateGenerator.js
@@ -0,0 +1,64 @@
+class NonprofitCertificateGenerator {
+    constructor(country) {
+        this.country = country;
+    }
+
+    generate12ACertificate(details) {
+        // Logic to generate 12A Certificate for India
+        // Use HTML5 Canvas API
+    }
+
+    generate80GCertificate(details) {
+        // Logic to generate 80G Certificate for India
+        // Use HTML5 Canvas API
+    }
+
+    generateNGODarpanID(details) {
+        // Logic to generate NGO Darpan ID for India
+        // Use HTML5 Canvas API
+    }
+
+    generateFCRACertificate(details) {
+        // Logic to generate FCRA Certificate for India
+        // Use HTML5 Canvas API
+    }
+
+    generateTrustDeed(details) {
+        // Logic to generate Trust Deed for India
+        // Use HTML5 Canvas API
+    }
+
+    generatePANCard(details) {
+        // Logic to generate PAN Card for India
+        // Use HTML5 Canvas API
+    }
+
+    generate501c3Certificate(details) {
+        // Logic to generate USA 501c3 Certificate
+        // Use HTML5 Canvas API
+    }
+
+    generateUKCharityRegistration(details) {
+        // Logic to generate UK Charity Registration
+        // Use HTML5 Canvas API
+    }
+
+    generateCanadaCRACertificate(details) {
+        // Logic to generate Canada CRA Certificate
+        // Use HTML5 Canvas API
+    }
+
+    generateAustraliaACNCCertificate(details) {
+        // Logic to generate Australia ACNC Certificate
+        // Use HTML5 Canvas API
+    }
+
+    renderCertificate(canvasId, certificateData) {
+        // Logic to render certificate on HTML5 Canvas
+    }
+}
+
+// Example usage:
+const generator = new NonprofitCertificateGenerator('India');
+const certificateDetails = { /* details for the certificate */ };
+generator.generate12ACertificate(certificateDetails);
